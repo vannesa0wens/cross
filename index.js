730939c4f90fe9f949a115d3fb33b1eb523714ec
Vannesa <140695617+vannesa0wens@users.noreply.github.com>
2024-02-15 20:32:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const counterDOM = document.getElementById('counter');  
 const endDOM = document.getElementById('end');  
 
-const scene = new THREE.Scene();
+
 
 const distance = 500;
 const camera = new THREE.OrthographicCamera( window.innerWidth/-2, window.innerWidth/2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000 );
