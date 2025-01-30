4dc4191521d7324c2864fe8df0e85d0fb63abc36
Vannesa <140695617+vannesa0wens@users.noreply.github.com>
2025-01-30 22:09:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let distance = 500;
 const camera = new THREE.OrthographicCamera( window.innerWidth/-2, window.innerWidth/2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 10000 );
 
 camera.rotation.x = 50*Math.PI/180;
-camera.rotation.y = 20*Math.PI/180;
-camera.rotation.z = 10*Math.PI/180;
+camera.rotation.y = 20*Math.PI/580;
+camera.rotation.z = 10*Math.PI/380;
 
 const initialCameraPositionY = -Math.tan(camera.rotation.x)*distance;
 const initialCameraPositionX = Math.tan(camera.rotation.y)*Math.sqrt(distance**2 + initialCameraPositionY**2);
